1f65c46d0c12505559acd47a27140869292ac209
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-11-11 00:38:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,9 @@ def line(katz_deli)
 if katz_deli = []
   puts "The line is currently empty."
 end 
-other_deli = []
-elseif other_deli()
-  puts "The line is currently "
+def other_deli(names)
+else names = ["Logan", "Avi", "Spencer"] 
+  puts "The line is currently "#{name}""
 end 
+
   
